21825d3b5b75211f380a2b277ab8b38ed4579e38
wochalei <1643527245@qq.com>
2022-06-30 21:34:00 +0800

    修改

--- src/DB/database.js
+++ src/DB/database.js
@@ -2,8 +2,8 @@ const axios = require('axios')
 const qs = require('querystring')
 const param = qs.stringify({
     grant_type: "client_credential",
-    appid: "wx35ad6b2491c12b38",
-    secret: '3ddc4051a04a7cb090d5ec462cadaa85'
+   /*  appid: "wx35ad6b2491c12b38",
+    secret: '3ddc4051a04a7cb090d5ec462cadaa85' */
 })
 
 let url =
